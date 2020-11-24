eda1f3650aec9e2808a59e3f4527022a369fd794
Willem Oldeman <Willy65@users.noreply.github.com>
2020-11-24 22:01:48 +0100

    Add files via upload
    Add some test scenarios.
    The script assumes that the programname is called "LineFeedCleaner"
    and that the program LineFeedCleaner resides in "../Release" (eclipse)
    
    Adjust settings to your own individual setup.

---
+++ Testing/testcleaner.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Testscript voor LineFeedCleaner  om te testen in verschillende scenario's
+#
+
+PROGDIR="../Release"
+PROGNAME="LineFeedCleaner"
+
+for FILE in \
+	"demo_perfect.txt" \
+	"demo_extra_lfs.txt" \
+	"demo_broken_lf.txt" \
+	"demo_linefeed.txt" \
+	"demo_double_bad_lf.txt"
+do
+	clear
+	echo $FILE
+	cat $FILE
+	echo
+	$PROGDIR/$PROGNAME $FILE "pattern" > tmp
+	RESULT=$?
+	echo
+	echo "Outputfile ="
+	cat tmp
+	echo
+	echo "Diff ="
+	diff $FILE tmp
+	echo
+	NUMLIN1=$(cat $FILE | wc -l)
+	NUMLIN2=$(cat tmp   | wc -l)
+	echo "Numlines original: $NUMLIN1"
+	echo "Numlines result  : $NUMLIN2"
+	echo "Result           : $RESULT"
+	printf "\nPress enter for next file"
+	read A
+done
+rm -f tmp
+
